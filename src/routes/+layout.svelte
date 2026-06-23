007d6b72a49670077e990737bc538989338cca9e
SikandarJODD <sikandar6174@gmail.com>
2026-06-23 09:05:17 +0530

    feat: base theme setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,35 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { activeElement, PressedKeys } from 'runed';
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import { browser } from '$app/env';
 
 	let { children } = $props();
+
+	let keys = new PressedKeys();
+	keys.onKeys(['d'], () => {
+		if (
+			activeElement.current?.localName === 'input' ||
+			activeElement.current?.localName === 'textarea'
+		)
+			return;
+		toggleMode();
+	});
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser
+			}
+		}
+	});
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<ModeWatcher defaultMode="dark" />
+<QueryClientProvider client={queryClient}>
+	{@render children()}
+</QueryClientProvider>
