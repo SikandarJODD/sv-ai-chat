f29c394da29a8ab47a26c16812ba3721ff3205d0
SikandarJODD <sikandar6174@gmail.com>
2026-06-25 07:52:36 +0530

    feat: basic prompt input from Svelte AI Elements

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Svelte AI Chat
 
-Simple AI Chat website - another one 😁
+Simple AI Chat website
+
+## Inspiration
+
+Inspired from Open AI Chat GPT
+
+## Tech Stack
+
+Frontend: Sveltekit
+DB: Neon DB
+ORM: Drizzle ORM
+Auth: Better Auth (Github, Google)
+AI: Tanstack AI 😁
---
+++ src/lib/components/chat/main-chat.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import * as PromptInput from '$lib/components/ai-elements/prompt-input';
+	import type { Message } from '$lib/components/ai-elements/prompt-input';
+	function handleSubmit(message: Message) {
+		console.log('Submitted message:', message.text);
+		console.log('Files', message.files);
+	}
+</script>
+
+<div class="h-full flex justify-center items-center">
+	<div>
+		<PromptInput.Root globalDrop maxFiles={2} class="w-xl shadow-none" onSubmit={handleSubmit}>
+			<PromptInput.Attachments>
+				{#snippet children(file)}
+					<PromptInput.Attachment data={file} />
+				{/snippet}
+			</PromptInput.Attachments>
+			<PromptInput.Body>
+				<PromptInput.Textarea placeholder="Ask anything..." />
+			</PromptInput.Body>
+			<PromptInput.Toolbar class="justify-end">
+				<PromptInput.Submit />
+			</PromptInput.Toolbar>
+		</PromptInput.Root>
+	</div>
+</div>
